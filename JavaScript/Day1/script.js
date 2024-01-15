d6bddc1aa1e89d7c3ba193c9cfdec32cbe37a2fc
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-15 12:33:39 +0000

    update script.js

--- JavaScript/Day1/script.js
+++ JavaScript/Day1/script.js
@@ -18,9 +18,9 @@ console.warn("Welcome to JavaScript");
 console.clear();
 
 // Ways of creating a variable
-let favouriteMeal = "pizza";
-const number = 5;
-var happyPlace = "Beach";
+let favouriteMeal = "pizza"; //let allows you to change the variable, has block-scope
+const number = 5; //has block-scope, it needs to be initialized when declared, value cannot be changed (read only)
+var happyPlace = "Beach"; //doesnt have block-scope, old way of declaring variable, not recommended
 
 
 if (favouriteMeal == "pizza") {
@@ -29,3 +29,30 @@ if (favouriteMeal == "pizza") {
 }
 
 console.log(number * 5);
+console.clear();
+
+//! ***  TASK ***
+ 
+ /*
+ 1. I would like you to Create a variable called fastFood and assign the value Pizza,
+    another variable called favColor with the value Red and a third
+    variable called favDrink with the value lemonade.
+ 
+ 2. Console log the favDrink variable to show it's value.
+ 
+ 3. Change the value of the favFood variable to "Pasta", console log it's value.
+ 
+ 4. Create a variable, call it whatever you want and assign the value of your brithday.*/
+
+
+let favFood = "Pizza";
+let favColor = "Red";
+let favDrink = "Lemonade";
+
+console.log(favDrink);
+
+favFood = "Pasta";
+console.log(favFood);
+
+const dob = "20/09/1993";
+console.log(dob);
