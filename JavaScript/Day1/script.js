b1bed5da9e0598fc8e4ae90a13a8691c8f8b2c2f
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-15 10:19:41 +0000

    Created hmtl and JS files
    added some console functions to scripts.js. Externally linked JS file to html file

---
+++ JavaScript/Day1/script.js
@@ -0,0 +1,22 @@
+// console.log("Welcome to JavaScript");
+
+// single line comment
+
+/* 
+Multi line comment
+*/
+
+/*console.log("Display a message to console");
+console.warn("Displays a warning message to console");
+console.error("displays a error message to console");
+alert("Creates a message box");
+console.clear(); // clears content of the console*/
+
+console.log("Welcome to JavaScript");
+console.error("Welcome to JavaScript");
+console.warn("Welcome to JavaScript");
+console.clear();
+
+let favouriteMeal = "pizza";
+const number = 5;
+var happyPlace = "Beach";
