ebef248974e6646072d7725c7025dcadf905e150
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-16 09:49:33 +0000

    Created day 2 folder
    added some variables to show different primitive data types

---
+++ JavaScript/Day2/script.js
@@ -0,0 +1,26 @@
+let num1 = 100; //number value
+let firstName = "Corné"; //string value
+console.log(typeof num1);
+console.log(typeof firstName);
+
+
+let cold = true; //boolean has two values - true, false
+console.log(typeof cold);
+
+if (cold) {
+    console.log("It is very cold!");
+}
+
+// null is used to declare a value as empty
+let a = null;
+console.log(a);
+//undefine is a variable which is not defined
+let b;
+console.log(b);
+
+/*undefined means a variable has been declared but has not yet been assigned a value, 
+whereas null is an assignment value, meaning that a variable has been declared and 
+given the value of null */
+
+let num2 = 1484718734817047170941847n; //n at the end of the number makes it a big integer(bigInt)
+console.log(typeof num2)
