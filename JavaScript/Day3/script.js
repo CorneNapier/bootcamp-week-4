9b056020d97c1ceea79aeed4352737e6d02acf04
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-17 12:14:40 +0000

    Update script.js
    Added what to do for tasks, and completed the tasks

--- JavaScript/Day3/script.js
+++ JavaScript/Day3/script.js
@@ -80,10 +80,10 @@ switch (day) {
 //switch statement 2
 
 let testScore = 80;
-//used true for checking condition
+//used true for checking condition - any condition that is true will execute
 switch (true) {
     case testScore >= 75:
-        console.log(`Distinction`)
+        console.log(`Score:${testScore},  Distinction`)
         break;
     case testScore >= 60:
         console.log(`Merit`);
@@ -94,5 +94,57 @@ switch (true) {
 
     default:
         console.log(`Fail`);
+        break;
+}
+console.clear()
+//!Task
+/*1:  Write a conditional statement to check a customer’s age at a bar. If the customer is under 18 log a message advising they are too young, otherwise ask what they would like to drink.
+2: Declare a variable called "Password". Write an if statement that checks how many characters are in the password, if the password has more than 8 characters log the password to the console, if the password has less than 8 characters log to the console that the password is too short.
+3: Create a variable that stores a number. Check whether the number is divisible by 3 or 5, if so log "This number is divisible by 3 or 5" to the console. Otherwise log an alternate message to the console.
+4: Create a variable that stores a number. If the number is divisible by 3, log "fizz" to the console. If the number is divisible by 5 log "buzz" to the console. If the number is divisible by both 3 and 5, log "fizz buzz" to the console. Otherwise log the number to the console.*/
+
+//Task 1
+if (age < 18) {
+    console.log(`Sorry, you are not allowed to buy any alcoholic drinks.`)
+} else {
+    console.log(`What drink would you like?`);
+}
+
+//Task 2
+let password = "whuaffarwwr";
+
+if (password.length >= 8) {
+    console.log(password);
+} else {
+    console.log(`Password is too short, add more characters`);
+}
+
+//Task 3
+let number = 30;
+
+if (number % 3 == 0 && number % 5 == 0) {
+    console.log(`Number is divisible by 3 and 5`);
+} else if (number % 3 == 0 || number % 5 == 0) {
+    console.log(`Number is divisible by 3 or 5`)
+} else {
+    console.log(`Number is not divisible by 3 or 5`);
+}
+
+//task 4
+let num2 = 30;
+
+switch (true) {
+    case num2 % 3 == 0 && num2 % 5 == 0:
+        console.log(`fizz buzz`);
+        break;
+    case num2 % 3 == 0:
+        console.log(`fizz`);
+        break;
+    case num2 % 5 == 0:
+        console.log(`buzz`);
+        break;
+
+    default:
+        console.log(num2);
         break;
 }
