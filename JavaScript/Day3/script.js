feb34fc4af257d2a2bf12a27774d6d6bd043546e
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-17 11:31:01 +0000

    update script.js
    added switch statement examples

--- JavaScript/Day3/script.js
+++ JavaScript/Day3/script.js
@@ -46,3 +46,53 @@ if (age >= 18) {
 } else {
     console.log("Sorry, you are not old enough to vote.");
 }
+console.clear();
+//switch statement - useful when you have one condition being checked against multiple cases
+
+let day = "Saturday";
+
+switch (day) {
+    case "Monday":
+        console.log("Happy Monday!");
+        break;
+    case "Tuesday":
+        console.log("Second day of the week!");
+        break;
+    case "Wednesday":
+        console.log("Halfway through the week!")
+        break;
+    case "Thursday":
+        console.log("Fourth day of the week!")
+        break;
+    case "Friday":
+        console.log("Yaaay it's Friday!");
+        break;
+    case "Saturday":
+    case "Sunday":
+        console.log("It is the weekend!");
+        break;
+
+    default:
+        console.log("Day not recognised, try again!");
+        break;
+}
+
+//switch statement 2
+
+let testScore = 80;
+//used true for checking condition
+switch (true) {
+    case testScore >= 75:
+        console.log(`Distinction`)
+        break;
+    case testScore >= 60:
+        console.log(`Merit`);
+        break;
+    case testScore >= 50:
+        console.log(`Pass`);
+        break;
+
+    default:
+        console.log(`Fail`);
+        break;
+}
