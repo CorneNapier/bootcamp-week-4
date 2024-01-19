6b14c0b09e5e57b4453829c186f34b39a55292bc
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-19 10:01:23 +0000

    Update script.js
    Added some more function examples

--- JavaScript/Day5/script.js
+++ JavaScript/Day5/script.js
@@ -9,3 +9,53 @@ function greeting() {
 
 //call or invoke a function
 greeting();
+
+//User login/logout function
+
+let userOnline = true;
+
+function userStatus() {
+
+    if (userOnline) {
+        console.log("User Signed In!");
+        console.log(userOnline);
+    } else {
+        console.log("User Signed Out!");
+        console.log(userOnline);
+    }
+
+}
+
+userStatus();
+
+//!Function with parameters/arguments
+
+//parameters are defined when we declare the function. Data that is expected.
+function atm(accountNumber, amount) {
+    console.log(`Account Number: ${accountNumber}. Amount: ${amount}`);
+}
+
+//Arguments are the data we pass to the function when we call it
+atm(135332553, 300);
+
+function sum(num1, num2) {
+    return num1+num2;
+}
+
+console.log(sum(20, 10));
+
+console.clear();
+
+//task
+function personGreeting(personName) {
+    console.log(`Hello ${personName}, how are you today?`);
+}
+
+personGreeting("Corné");
+
+
+
+//! Scope in JavaScript
+  //block scope
+  //local scope
+  //global scope
