1ac625608c07a646631e735dde34b47ab3ad4c66
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-19 11:35:43 +0000

    update scripts.js
    Added arrow function notes and examples. Added task descriptions

--- JavaScript/Day5/script.js
+++ JavaScript/Day5/script.js
@@ -47,15 +47,56 @@ console.log(sum(20, 10));
 console.clear();
 
 //task
+/*let personName = prompt("What is you name?")
+
 function personGreeting(personName) {
-    console.log(`Hello ${personName}, how are you today?`);
+    alert(`Hello ${personName}, how are you today?`);
 }
 
-personGreeting("Corné");
-
+personGreeting(personName);*/
 
 
 //! Scope in JavaScript
   //block scope
+    //let, const
+
   //local scope
+    //var, let, const
+
   //global scope
+    //if you declare a variable outside a function, it has global scope.
+
+
+  //!Functions that call another functions
+
+  function double(num) {
+    return num*2;
+  }
+
+  function quadruple(num2) {
+    return double(num2)*2;
+  }
+
+  //Function can be hoisted - Called before they are declared
+  scope();
+
+  function scope() {
+    console.log("Example of hoisting.");
+  }
+
+  //Arrow function
+    //omit the return statement - it has explicit return
+    //omit the function keyword
+    //when there is only one parameter, brackets is optional.
+let add2 = num => num * 2;
+let multiply = (x, y) => x * y;
+
+
+/*
+!Tasks:
+1: Write a function that takes in a first name and a surname as arguments and returns a message greeting them.
+2: Write a function that takes in a string as an argument and sorts the string into alphabetical order and returns the result.
+3: Write a cash machine / atm function that takes in a withdrawal amount and a pin number as an argument and compares the pin and withdrawal amount against a stored pin and account balance. If the pin matches and the balance is sufficient approve the transaction, if not decline the transaction.
+*/
+
+
