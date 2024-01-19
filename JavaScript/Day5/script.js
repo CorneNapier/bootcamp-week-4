58fd639f22949ebb071e4fa53ee6f11399a36072
CorneNapier <CorneNapier.bootcamp@justit.co.uk>
2024-01-19 09:36:57 +0000

    Created day 5 folder
    Updated html and script file. Added a function with some notes

---
+++ JavaScript/Day5/script.js
@@ -0,0 +1,11 @@
+//!Function
+//function is a block of code used to perform a specific task
+//function is executed when something invokes it (calls it)
+
+//declaring a function
+function greeting() {
+    console.log("Hello, welcome to JavaScript!");
+}
+
+//call or invoke a function
+greeting();
